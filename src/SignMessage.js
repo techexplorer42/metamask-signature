a2ce40596f0297e100058c8966e58e70d04ca9a3
aminem2019 <aminemw29@gmail.com>
2022-10-18 17:13:45 +0000

    Generate and check zip files! ( more cleaning needed)

--- src/SignMessage.js
+++ src/SignMessage.js
@@ -4,83 +4,94 @@ import ErrorMessage from "./ErrorMessage";
 import JSZip from 'jszip';
 import FileSaver from 'file-saver';
 
-const signMessage = async ({ setError, message }) => {
-  try {
-    console.log({ message });
-    if (!window.ethereum)
-      throw new Error("No crypto wallet found. Please install it.");
-    // ("eth_requestAccounts");
-    await window.ethereum.request ({ method: 'eth_requestAccounts'}).then((result) => {
-    // The result varies by RPC method.
-    // For example, this method will return a transaction hash hexadecimal string on success.
-  })
-  .catch((error) => {
-    // If the request fails, the Promise will reject with an error.
-  });
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const signer = provider.getSigner();
-    const signature = await signer.signMessage(message);
-    const address = await signer.getAddress();
+const signMessage = async ({
+    setError,
+    message
+}) => {
+    try {
+        console.log({
+            message
+        });
+        if (!window.ethereum)
+            throw new Error("No crypto wallet found. Please install it.");
+        // ("eth_requestAccounts");
+        await window.ethereum.request({
+                method: 'eth_requestAccounts'
+            }).then((result) => {
+                // The result varies by RPC method.
+                // For example, this method will return a transaction hash hexadecimal string on success.
+            })
+            .catch((error) => {
+                // If the request fails, the Promise will reject with an error.
+            });
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const signature = await signer.signMessage(message);
+        const address = await signer.getAddress();
 
-    return {
-      message,
-      signature,
-      address
-    };
-  } catch (err) {
-    setError(err.message);
-  }
+        return {
+            message,
+            signature,
+            address
+        };
+    } catch (err) {
+        setError(err.message);
+    }
 };
 
 export default function SignMessage() {
-  const resultBox = useRef();
-  const [signatures, setSignatures] = useState([]);
-  const [error, setError] = useState();
+    const resultBox = useRef();
+    const [signatures, setSignatures] = useState([]);
+    const [error, setError] = useState();
 
     const handleFileInput = async (e) => {
-  	const file = e.target.files[0];
-  	const fileName = file.name;
-	console.log("File handling");
-	console.log(Object.keys(file));
-	const status = document.getElementById('status');
-	const output = document.getElementById('output');
-	const signature = document.getElementById('signature');
-	status.textContent = "Loaded file is " + file.name + " size is: " + file.size;
+        const file = e.target.files[0];
+        const fileName = file.name;
+        console.log("File handling");
+        console.log(Object.keys(file));
+        const status = document.getElementById('status');
+        const output = document.getElementById('output');
+        const signature = document.getElementById('signature');
+        status.textContent = "Loaded file is " + file.name + " size is: " + file.size;
 
-	  async function handleFile(){
-		const fileContent = event.target.result;
-		output.innerText = fileContent;
-		const sig = await signMessage({ setError: console.log, message :  fileContent });
-		signature.innerText = sig.signature;
+        async function handleFile() {
+            const fileContent = event.target.result;
+            output.innerText = fileContent;
+            const sig = await signMessage({
+                setError: console.log,
+                message: fileContent
+            });
+            signature.innerText = sig.signature;
 
-		const zip = new JSZip();
-		// zip.file(fileName, fileContent);
-		zip.file(fileName, fileContent);
-		zip.file('signature.txt', sig.signature);
-		zip.file('address.txt', sig.address);
-		zip.generateAsync({ type: 'blob' }).then(function (content) {
-		    FileSaver.saveAs(content, 'signed_document.zip');
-	    	});
-	  }
-	  const reader = new FileReader();
-	  reader.addEventListener('load', handleFile);
-	  reader.readAsText(file);
+            const zip = new JSZip();
+            // zip.file(fileName, fileContent);
+            zip.file(fileName, fileContent);
+            zip.file('signature.txt', sig.signature);
+            zip.file('address.txt', sig.address);
+            zip.generateAsync({
+                type: 'blob'
+            }).then(function(content) {
+                FileSaver.saveAs(content, 'signed_document.zip');
+            });
+        }
+        const reader = new FileReader();
+        reader.addEventListener('load', handleFile);
+        reader.readAsText(file);
     }
 
-  return (
+    return (
     <div className="credit-card w-full shadow-lg mx-auto rounded-xl bg-white">
         <main className="mt-4 p-4">
           <h1 className="text-xl font-semibold text-gray-700 text-center">
             Sign a file
           </h1>
-	</main>
-    <input className="btn btn-primary submit-button focus:ring focus:outline-none w-full" color="black" type="file" id="file-selector" onChange={handleFileInput} />
-    <p id="status"></p>
-    <div>
-      <p id="output"></p>
-      <p id="signature"></p>
+			  </main>
+				<input className="btn btn-primary submit-button focus:ring focus:outline-none w-full" color="black" type="file" id="file-selector" onChange={handleFileInput} />
+				<p id="status"></p>
+				<div>
+					<p id="output"></p>
+					<p id="signature"></p>
+				</div>
     </div>
-    </div>
-
-  );
+    );
 }
--- src/VerifyMessage.js
+++ src/VerifyMessage.js
@@ -5,136 +5,110 @@ import SuccessMessage from "./SuccessMessage";
 import JSZip from 'jszip';
 import FileSaver from 'file-saver';
 
-const verifyMessage = async ({ message, address, signature }) => {
-  try {
-    const signerAddr = await ethers.utils.verifyMessage(message, signature);
-    if (signerAddr !== address) {
-      return false;
-    }
+const verifyMessage = async ({
+    message,
+    address,
+    signature
+}) => {
+    try {
+        const signerAddr = await ethers.utils.verifyMessage(message, signature);
+        if (signerAddr !== address) {
+            return false;
+        }
 
-    return true;
-  } catch (err) {
-    console.log(err);
-    return false;
-  }
+        return true;
+    } catch (err) {
+        console.log(err);
+        return false;
+    }
 };
 
 export default function VerifyMessage() {
-  const [error, setError] = useState();
-  const [successMsg, setSuccessMsg] = useState();
-  var fileblob;
+    const [error, setError] = useState();
+    const [successMsg, setSuccessMsg] = useState();
+    var fileblob;
     const handleFileInputVerify = async (e) => {
-  	const file = e.target.files[0];
-	console.log("File handling");
-	const verifyfilestatus = document.getElementById('verifyfilestatus');
-	verifyfilestatus.textContent = "Loaded file size is: "+file.size;
-	fileblob = file;
-	//verifyfilestatus.textContent = 'Error: The File.type property does not appear to be supported on this browser.';
+        const file = e.target.files[0];
+        const verifyfilestatus = document.getElementById('verifyfilestatus');
+        verifyfilestatus.textContent = "Loaded file size is: " + file.size;
+        fileblob = file;
     }
 
-  const handleVerification = async (e) => {
-    e.preventDefault();
-    const data = new FormData(e.target);
-    setSuccessMsg();
-    setError();
+    const handleVerification = async (e) => {
+        e.preventDefault();
+        const data = new FormData(e.target);
+        setSuccessMsg();
+        setError();
 
-	var zip = new JSZip();
-	zip.loadAsync(fileblob)
-	.then(async function (zip) {
-	    var zfiles = zip.files;
-	    var fileSignature = "signature.txt";
-	    var fileDoc = "";
-	    var fileAddr = "address.txt";
-	    if (zip.files.length != 3) {
-		console.log("there is more or less (" + zip.files.length + ") than 3 files");
-	    }
-	    // TODO check that fileSignature and fileAddr exists
-	    console.log(zfiles);
-	    //console.log(zfiles[1].name);
-	    //console.log(zfiles[0].indexOf(fileSignature));
-	    //console.log(beasts.indexOf(fileAddr));
-	    zip.forEach((relPath, file) => {
-		console.log(relPath);
-	        if (relPath != fileAddr  && relPath != fileSignature) {
-			fileDoc = relPath;
-       		}
-      	    });
+        var zip = new JSZip();
+        zip.loadAsync(fileblob)
+            .then(async function(zip) {
+                var zfiles = zip.files;
+                var fileSignature = "signature.txt";
+                var fileDoc = "";
+                var fileAddr = "address.txt";
+                console.log(zfiles);
+								var fNum = 0;
+                zip.forEach((relPath, file) => {
+                    console.log(relPath);
+                    if (relPath != fileAddr && relPath != fileSignature) {
+                        fileDoc = relPath;
+                    }
+										fNum += 1;
+                });
+                // TODO check that fileSignature and fileAddr exists
+                if (fNum != 3) {
+                    console.log("there is more or less (" + fNum + ") than 3 files");
+                    setError("This archive does not contain 3 files as expected!");
+										return;
+                }
+                var doc = 'doc';
+                var addr = 'add';
+                var signature = 'sig';
+                signature = await zip.file(fileSignature).async("text");
+                doc = await zip.file(fileDoc).async("text");
+                addr = await zip.file(fileAddr).async("text");
+                console.log(signature);
+                console.log(addr);
+                const isValid = await verifyMessage({
+                    setError,
+                    message: doc,
+                    address: addr,
+                    signature: signature
+                });
 
-	    var doc = 'doc';
-	    var addr = 'add';
-	    var signature = 'sig';
-	    signature = await zip.file(fileSignature).async("text");
-	    doc = await zip.file(fileDoc).async("text");
-	    addr = await zip.file(fileAddr).async("text");
-	    //var addr = await zip.file(fileAddr).async("string");
-	    //var signature = await zip.file(fileSignature).async("string");
-	    console.log(signature);
-	    console.log(addr);
-    const isValid = await verifyMessage({
-      setError,
-      message: doc,
-      address: addr,
-      signature: signature
-    });
+                if (isValid) {
+                    setSuccessMsg("Signature is valid!");
+                } else {
+                    setError("Invalid signature");
+                }
+            })
+    };
 
-    if (isValid) {
-      setSuccessMsg("Signature is valid!");
-	console.log("Signature is valid!!!")
-     
-    } else {
-      setError("Invalid signature");
-      console.log("Invalid signature");
-    }
-
-  	    //const reader = new FileReaderSync();
-  	    //reader.addEventListener('load', handleFile);
-  	    //mainFileContent = reader.readAsText(zip.files[0]);
-	})
-  };
-
-  return (
-    <form className="m-4" onSubmit={handleVerification}>
-      <div className="credit-card w-full shadow-lg mx-auto rounded-xl bg-white">
-        <main className="mt-4 p-4">
-          <h1 className="text-xl font-semibold text-gray-700 text-center">
-            Verify signature
-          </h1>
-          <div className="">
-            <div className="my-3">
-    		<input className="textarea w-full h-24 textarea-bordered focus:ring focus:outline-none" type="file" id="file-selector" onChange={handleFileInputVerify} />
-    		<p id="verifyfilestatus"></p>
-            </div>
-            <div className="my-3">
-              <textarea
-                type="text"
-                name="signature"
-                className="textarea w-full h-24 textarea-bordered focus:ring focus:outline-none"
-                placeholder="Signature"
-              />
-            </div>
-            <div className="my-3">
-              <input
-                type="text"
-                name="address"
-                className="textarea w-full input input-bordered focus:ring focus:outline-none"
-                placeholder="Signer address"
-              />
-            </div>
-          </div>
-        </main>
-        <footer className="p-4">
-          <button
-            type="submit"
-            className="btn btn-primary submit-button focus:ring focus:outline-none w-full"
-          >
-            Verify signature
-          </button>
-        </footer>
-        <div className="p-4 mt-4">
-          <ErrorMessage message={error} />
-          <SuccessMessage message={successMsg} />
-        </div>
-      </div>
-    </form>
-  );
+    return (
+			<form className="m-4" onSubmit={handleVerification}>
+				<div className="credit-card w-full shadow-lg mx-auto rounded-xl bg-white">
+					<main className="mt-4 p-4">
+						<h1 className="text-xl font-semibold text-gray-700 text-center">
+							Verify signature
+						</h1>
+						<div className="">
+							<div className="my-3">
+					<input style={{color: "red"}}  type="file" id="file-selector" onChange={handleFileInputVerify} />
+					<p id="verifyfilestatus"></p>
+							</div>
+						</div>
+					</main>
+					<footer className="p-4">
+						<button type="submit" className="btn btn-primary submit-button focus:ring focus:outline-none w-full">
+							Verify Signed Archive
+						</button>
+					</footer>
+					<div className="p-4 mt-4">
+						<ErrorMessage message={error} />
+						<SuccessMessage message={successMsg} />
+					</div>
+				</div>
+			</form>
+    );
 }
